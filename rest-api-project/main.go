6f7e009f47cc2268d9d826c57a13b24a9812f70c
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-08 21:41:32 +0600

    updated app

--- rest-api-project/main.go
+++ rest-api-project/main.go
@@ -1,38 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
+
+	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/cmd/api"
 )
 
-type Hello struct {
-	Ok      bool   `json:"ok"`
-	Message string `json:"message"`
-}
-
-func NewHello(ok bool, message string) *Hello {
-	return &Hello{
-		Ok:      ok,
-		Message: message,
-	}
-}
-
 func main() {
-	router := http.NewServeMux()
+
 	port := "8080"
 	fmt.Println("server is running on port " + port)
-	router.HandleFunc("GET /", helloApi)
-	err := http.ListenAndServe(":"+port, router)
+
+	server := api.NewServer(port)
+
+	err := server.Run()
+
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func helloApi(w http.ResponseWriter, r *http.Request) {
-	encoder := json.NewEncoder(w)
-	hello := NewHello(true, "server is running")
-	w.Header().Add("Content-Type", "application/json")
-	encoder.Encode(&hello)
 }
