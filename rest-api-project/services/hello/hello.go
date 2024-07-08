6f9570fe256348dbf06f7b20d347e76b196882dd
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-08 21:58:07 +0600

    refractor hello api

--- rest-api-project/services/hello/hello.go
+++ rest-api-project/services/hello/hello.go
@@ -1,5 +1,10 @@
 package hello
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type Hello struct {
 	Ok      bool   `json:"ok"`
 	Message string `json:"message"`
@@ -10,4 +15,11 @@ func NewHello(ok bool, message string) *Hello {
 		Ok:      ok,
 		Message: message,
 	}
+}
+
+func HelloApi(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	hello := NewHello(true, "server is running")
+	w.Header().Add("Content-Type", "application/json")
+	encoder.Encode(&hello)
 }
