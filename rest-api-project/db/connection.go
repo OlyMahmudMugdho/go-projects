35f9fb7649b11b405078caa29fa9f53b3c9e207a
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-10 13:59:06 +0600

    connection function

---
+++ rest-api-project/db/connection.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/types"
+	_ "github.com/lib/pq"
+)
+
+func ConnectionString(p types.PostgresConfig) string {
+	return "postgresql://" + p.Username + ":" + p.Password + "@" + p.Host + ":" + string(p.Port) + p.DB + "?=sslmode=" + p.Sslmode
+}
+
+func Connect(config types.PostgresConfig) (conn *sql.DB, err error) {
+	db, err := sql.Open("postgres", ConnectionString(config))
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("db connected")
+	}
+
+	return db, err
+}
