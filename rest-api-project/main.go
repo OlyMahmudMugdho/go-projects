a0856e5920722d14bc2ed7637d061bb602a8bbad
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-11 20:51:09 +0600

    updated the app

--- rest-api-project/main.go
+++ rest-api-project/main.go
@@ -5,30 +5,28 @@ import (
 	"log"
 
 	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/cmd/api"
-	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/db"
-	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/types"
+	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/utils/dotenv"
 )
 
 func main() {
 
-	port := "8080"
-	fmt.Println("server is running on port " + port)
+	dotenv.Load()
 
-	cfg := &types.PostgresConfig{
-		Username: "postgres",
-		Password: "mysecretpassword",
-		Host:     "localhost",
-		Port:     5432,
-		DB:       "dummy",
-		Sslmode:  "disable",
+	vars := make(map[string]string)
+
+	vars["foo"] = "bar"
+
+	for key, value := range vars {
+		fmt.Println(key + " : " + value)
 	}
 
-	_, err := db.Connect(*cfg)
+	port := "8080"
+	fmt.Println("server is running on port " + port)
 
 	server := api.NewServer(port)
 	error := server.Run()
 
 	if error != nil {
-		log.Fatal(err)
+		log.Fatal(error)
 	}
 }
