23fd42cb2a09bedfd339bf63aceb6c34bfff7278
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-11 21:00:16 +0600

    removed previous dotenv codes

--- rest-api-project/main.go
+++ rest-api-project/main.go
@@ -5,12 +5,9 @@ import (
 	"log"
 
 	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/cmd/api"
-	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/utils/dotenv"
 )
 
 func main() {
-
-	dotenv.Load()
 
 	vars := make(map[string]string)
 
