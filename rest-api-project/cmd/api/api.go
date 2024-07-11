2e70155498cc2eb714c27f25936724e35db5650b
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-11 20:50:58 +0600

    separated db connection

--- rest-api-project/cmd/api/api.go
+++ rest-api-project/cmd/api/api.go
@@ -1,17 +1,34 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/db"
 	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/services/hello"
+	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/types"
 )
 
 type Server struct {
 	port string
+	cfg  types.PostgresConfig
 }
 
 func NewServer(port string) *Server {
-	return &Server{port: port}
+	cfg := &types.PostgresConfig{
+		Username: "postgres",
+		Password: "mysecretpassword",
+		Host:     "localhost",
+		Port:     5432,
+		DB:       "dummy",
+		Sslmode:  "disable",
+	}
+
+	_, err := db.Connect(*cfg)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return &Server{port: port, cfg: *cfg}
 }
 
 func (s *Server) Run() error {
