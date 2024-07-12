edb8071c42406a788f22da8db78b15173e30f1a5
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-12 12:52:07 +0600

    Register User controller

--- rest-api-project/services/users/routes.go
+++ rest-api-project/services/users/routes.go
@@ -1,6 +1,12 @@
 package users
 
-import "github.com/OlyMahmudMugdho/go-projects/rest-api-project/types"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/types"
+)
 
 type UserHandler struct {
 	store types.UserStore
@@ -8,4 +14,17 @@ type UserHandler struct {
 
 func NewUserHandler(store types.UserStore) *UserHandler {
 	return &UserHandler{store: store}
+}
+
+func (u *UserHandler) RegisterUser(w http.ResponseWriter, r *http.Request) {
+	user := new(types.User)
+	json.NewDecoder(r.Body).Decode(&user)
+
+	error := u.store.CreateUser(*user)
+	if error != nil {
+		log.Fatal(error)
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(user)
 }
