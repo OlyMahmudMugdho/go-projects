d985e0552a2d2f40acae96f3bb4a581cb9f89ace
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-12 15:41:18 +0600

    connection problem fixed

--- rest-api-project/cmd/api/api.go
+++ rest-api-project/cmd/api/api.go
@@ -1,12 +1,15 @@
 package api
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/db"
 	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/services/hello"
+	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/services/users"
 	"github.com/OlyMahmudMugdho/go-projects/rest-api-project/types"
 	"github.com/joho/godotenv"
 )
@@ -14,6 +17,7 @@ import (
 type ApiServer struct {
 	port string
 	cfg  types.PostgresConfig
+	db   *sql.DB
 }
 
 func NewServer(port string) *ApiServer {
@@ -23,7 +27,7 @@ func NewServer(port string) *ApiServer {
 		log.Fatal(envError)
 	}
 
-	cfg := &types.PostgresConfig{
+	var cfg = &types.PostgresConfig{
 		Username: os.Getenv("PG_USERNAME"),
 		Password: os.Getenv("PG_PASSWORD"),
 		Host:     os.Getenv("PG_HOST"),
@@ -32,11 +36,24 @@ func NewServer(port string) *ApiServer {
 		Sslmode:  os.Getenv("PG_SSLMODE"),
 	}
 
-	_, err := db.Connect(*cfg)
+	var conn string = db.ConnectionString(*cfg)
+
+	db, err := sql.Open("postgres", conn)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	return &ApiServer{port: port, cfg: *cfg}
+
+	error := db.Ping()
+
+	if error != nil {
+		log.Fatal(error)
+		fmt.Println("connection failed")
+	} else {
+		fmt.Println("connected to db")
+	}
+
+	return &ApiServer{port: port, cfg: *cfg, db: db}
 }
 
 func (s *ApiServer) Run() error {
@@ -44,6 +61,13 @@ func (s *ApiServer) Run() error {
 
 	helloHandler := hello.NewHello()
 	helloHandler.RegisterRoutes(router)
+
+	userStore := users.NewStore(s.db)
+	userStore.CreateDatabase()
+	// userStore.ReadSchema()
+	// userHandler := users.NewUserHandler(userStore)
+
+	// userHandler.RegisterRoutes(router)
 
 	return http.ListenAndServe(":"+s.port, router)
 }
