0dab665f207052dc0243c19df3b28ddb6824e0c2
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-21 20:55:20 +0600

    base64 decode

--- hashing/main.go
+++ hashing/main.go
@@ -7,9 +7,16 @@ import (
 
 func main() {
 	var hash string = base64Encode("mugdho")
-	fmt.Println(hash)
+	fmt.Println("hashed string : " + hash)
+	var decoded string = base64Decode(hash)
+	fmt.Println("decoded string : ", decoded)
 }
 
 func base64Encode(plain string) string {
 	return base64.RawStdEncoding.EncodeToString([]byte(plain))
+}
+
+func base64Decode(hash string) string {
+	decoded, _ := base64.RawStdEncoding.DecodeString(hash)
+	return string(decoded)
 }
