59eb2e6581f672584ebea5e698cf27edad2a87de
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-21 18:07:58 +0600

    made a Logger middleware

--- middleware/main.go
+++ middleware/main.go
@@ -8,9 +8,17 @@ import (
 
 func main() {
 	PORT := ":8080"
-	http.HandleFunc("GET /", SimpleApi)
+	handlerFn := http.HandlerFunc(SimpleApi)
+	http.Handle("GET /", Logger(handlerFn))
 	log.Printf("server is running on port %v \n", PORT)
 	log.Fatal(http.ListenAndServe(PORT, nil))
+}
+
+func Logger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%v %v \n", r.Method, r.URL.Path)
+		handler.ServeHTTP(w, r)
+	})
 }
 
 func SimpleApi(w http.ResponseWriter, r *http.Request) {
